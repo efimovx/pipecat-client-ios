d209416e5b3fb88704a23e5515d38b6983457b03
Paca Waca <paca.waca.x@gmail.com>
2026-03-18 16:37:37 +0100

    Update Package.swift
    - bump SPM toolkit to 6.0
    - bump min iOS version to 18
    - bump min macOS version to 15

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "PipecatClientIOS",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v17),
+        .macOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -22,5 +23,8 @@ let package = Package(
         .testTarget(
             name: "PipecatClientIOSTests",
             dependencies: ["PipecatClientIOS"]),
+    ],
+    swiftLanguageModes: [
+        .v5
     ]
 )
