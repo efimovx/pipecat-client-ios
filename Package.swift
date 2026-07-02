81aaa42e2d7f93671f93f06845517e03141ceab6
Kirill Efimov <mail@efimov.co>
2026-07-02 23:41:52 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,29 +1,24 @@
 // swift-tools-version:6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "PipecatClientIOS",
+    name: "PipecatClientIOSTV",
     platforms: [
         .iOS(.v17),
         .macOS(.v15),
         .tvOS(.v17),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "PipecatClientIOS",
-            targets: ["PipecatClientIOS"]),
+            name: "PipecatClientIOSTV",
+            targets: ["PipecatClientIOSTV"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PipecatClientIOS"),
+            name: "PipecatClientIOSTV"),
         .testTarget(
             name: "PipecatClientIOSTests",
-            dependencies: ["PipecatClientIOS"]),
+            dependencies: ["PipecatClientIOSTV"]),
     ],
     swiftLanguageModes: [
         .v5
